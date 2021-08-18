0bd6110e898aa6447f07e3a0cfbaa6fe4be40603
monik43 <79196412+monik43@users.noreply.github.com>
2021-08-18 11:48:48 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from . import mrp_repair
 from . import stock_picking
 from . import stock_move
 from . import stock_production_lot
-from . import mrp_repair
 from . import mail_activity
 from . import purchase
 from . import create_purchaseorder_mrp
