b184fb582ceaac421ed3fa5d391fda1cac98707f
monik43 <monimons666@gmail.com>
2021-08-30 11:32:31 +0200

    prueba creacion rep user, basic

--- models/reparation.py
+++ models/reparation.py
@@ -12,16 +12,17 @@ class reparation(models.Model):
     ticket = fields.Many2one('helpdesk.ticket')
     date = fields.Datetime("Fecha")
     RMA = fields.Char('RMA')
-    reparation_test_user = fields.One2many('cl.reparation.test','reparation', 'Test usuario')
-    reparation_test_basic = fields.One2many('cl.reparation.test','reparation', 'Test básico')
+    reparation_test_user = fields.One2many('cl.reparation.test','ureparation', 'Test usuario')
+    reparation_test_basic = fields.One2many('cl.reparation.test','breparation', 'Test usuario')
     test_pasado = fields.Boolean()
 
 
 class reparation_test(models.Model):
     _name = 'cl.reparation.test'
 
-    reparation = fields.One2many('cl.reparation','reparation_test', 'Reparacion') 
-    tname = fields.Char("Test                       ", readonly="True")
+    ureparation = fields.One2many('cl.reparation','reparation_test', 'Reparacion')
+    breparation = fields.One2many('cl.reparation','reparation_test', 'Reparacion')
+    name = fields.Char("Test                       ", readonly="True")
     notes = fields.Char("Observaciones")
     yes = fields.Boolean("Si")
     no = fields.Boolean("No")
--- wizard/create_reparation.py
+++ wizard/create_reparation.py
@@ -14,16 +14,19 @@ class createclreparation_mrp(models.TransientModel):
     origen_hdt = fields.Many2one(
         'helpdesk.ticket', 'Ticket reparación / Incidencia')
     tecnico_rep = fields.Many2one(
-        'res.users', 'Técnico', domain="[('share','=',False)]", default=lambda self: self.env.user.id)
-    origen_rep = fields.Many2one('mrp.repair', 'Reparación')
-    date = fields.Datetime("Fecha", default=lambda self: fields.datetime.now())
+        'res.users', 'Técnico', domain="[('share','=',False)]",
+         default=lambda self: self.env.user.id)
+    origen_rep = fields.Many2one(
+        'mrp.repair', 'Reparación')
+    date = fields.Datetime(
+        "Fecha", default=lambda self: fields.datetime.now())
     RMA = fields.Char('RMA')
     usr_credentials = fields.Many2one(
         'cl.user.credentials', 'Credenciales test usuario')
     reparation_test_basic = fields.One2many(
-        'getmrp.data.basic', 'new_line_id_b', 'Test')
+        'getmrp.data', 'new_line_id', 'Test')
     reparation_test_user = fields.One2many(
-        'getmrp.data.user', 'new_line_id_u', 'Test')
+        'getmrp.data', 'new_line_id', 'Test')
     product = fields.Many2one('product.product', 'Producto a reparar')
 
     def _test(self):
@@ -33,23 +36,17 @@ class createclreparation_mrp(models.TransientModel):
     def action_create_cl_reparation(self):
         self.ensure_one()
         res1 = self.env['cl.reparation'].browse(self._context.get('id', []))
-        res2 = self.env['cl.reparation.test.user'].browse(
-            self._context.get('id', []))
-        res3 = self.env['cl.reparation.test.basic'].browse(
-            self._context.get('id', []))
-        datamrp = self.env['mrp.repair'].browse(
-            self._context.get('active_ids', []))
+        test = self.env['cl.reparation.test'].browse(self._context.get('id', []))
+        datamrp = self.env['mrp.repair'].browse(self._context.get('active_ids', []))
         for data in self.reparation_test_user:
-            res2.create({
-                'tname': data.tname,
+            test.create({
+                'name': data.name,
                 'notes': data.notes,
                 'yes': data.yes,
                 'no': data.no
             })
-
-        for data in self.reparation_test_basic:
-            res3.create({
-                'tname': data.tname,
+            test.create({
+                'name': data.name,
                 'notes': data.notes,
                 'yes': data.yes,
                 'no': data.no
@@ -62,60 +59,45 @@ class createclreparation_mrp(models.TransientModel):
             'ticket': self.origen_hdt,
             'date': self.date,
             'RMA': self.RMA,
-            'reparation_test_user': res2,
-            'reparation_test_basic': res3
+            #'reparation_test_user': test,
+            #'reparation_test_basic': res3
         })
         return res1
 
     @api.multi
     def tprint(self):
         for line in self.reparation_test_basic:
-            print(line.tname)
+            print(line.name)
             print(line.notes)
 
     @api.model
     def default_get(self, fields):
         res = super(createclreparation_mrp, self).default_get(fields)
-        data = self.env['mrp.repair'].browse(
-            self._context.get('active_ids', []))
+        data = self.env['mrp.repair'].browse(self._context.get('active_ids', []))
 
         if data.product_id.id in (3412, 1279, 3405, 104, 1227, 242, 3379, 19, 400, 3165, 403, 3102, 3247, 1276, 3365, 3364, 3086, 297, 324, 330):
             print("/"*50)
-            res.update({'reparation_test_basic': [(0, 0, {'tname': 'WIFI'}), (0, 0, {'tname': 'Teclado'}), (0, 0, {'tname': 'Touchpad'}), (0, 0, {'tname': 'Pantalla táctil (Si lo és)'}), (0, 0, {'tname': 'Prueba carga (cargador original)'}), (0, 0, {
-                'tname': 'Prueba de carga (superior al 10%) 5% D 5% IZ'}), (0, 0, {'tname': 'Tornillos'}), (0, 0, {'tname': 'Embalaje'}), (0, 0, {'tname': 'Modo tablet (Táctil y que funcione KB y TP)'}), (0, 0, {'tname': 'Equipo de sustitución'})]})
+            res.update({'reparation_test_basic': [(0, 0, {'name': 'WIFI'}), (0, 0, {'name': 'Teclado'}), (0, 0, {'name': 'Touchpad'}), (0, 0, {'name': 'Pantalla táctil (Si lo és)'}), (0, 0, {'name': 'Prueba carga (cargador original)'}), (0, 0, {
+                'name': 'Prueba de carga (superior al 10%) 5% D 5% IZ'}), (0, 0, {'name': 'Tornillos'}), (0, 0, {'name': 'Embalaje'}), (0, 0, {'name': 'Modo tablet (Táctil y que funcione KB y TP)'}), (0, 0, {'name': 'Equipo de sustitución'})]})
         else:
-            res.update({'reparation_test_basic': [(0, 0, {'tname': 'WIFI'}), (0, 0, {'tname': 'Teclado'}), (0, 0, {'tname': 'Touchpad'}), (0, 0, {'tname': 'Prueba carga (cargador original)'}), (0, 0, {
-                'tname': 'Prueba de carga (superior al 10%) 5% D 5% IZ'}), (0, 0, {'tname': 'Tornillos'}), (0, 0, {'tname': 'Embalaje'}), (0, 0, {'tname': 'Equipo de sustitución'})]})
+            res.update({'reparation_test_basic': [(0, 0, {'name': 'WIFI'}), (0, 0, {'name': 'Teclado'}), (0, 0, {'name': 'Touchpad'}), (0, 0, {'name': 'Prueba carga (cargador original)'}), (0, 0, {
+                'name': 'Prueba de carga (superior al 10%) 5% D 5% IZ'}), (0, 0, {'name': 'Tornillos'}), (0, 0, {'name': 'Embalaje'}), (0, 0, {'name': 'Equipo de sustitución'})]})
 
-        res.update({'reparation_test_user': [(0, 0, {'tname': 'Battery Test'}), (0, 0, {'tname': 'Cámara #1 (1ª opción web: probar cámara)'}), (0, 0, {
-                   'tname': 'Cámara #2'}), (0, 0, {'tname': 'Micrófono (1ª opción web: probar micrófono)'}), (0, 0, {'tname': 'Audio (videos YouTube etc)'})]})
+        res.update({'reparation_test_user': [(0, 0, {'name': 'Battery Test'}), (0, 0, {'name': 'Cámara #1 (1ª opción web: probar cámara)'}), (0, 0, {
+                   'name': 'Cámara #2'}), (0, 0, {'name': 'Micrófono (1ª opción web: probar micrófono)'}), (0, 0, {'name': 'Audio (videos YouTube etc)'})]})
 
         return res
 
 
 class getmrpdata(models.TransientModel):
-    _name = 'getmrp.data.user'
+    _name = 'getmrp.data'
     _description = "Get MRP Repair user Data"
 
-    new_line_id_u = fields.Many2one('create.clreparation_mrp')
+    new_line_id = fields.Many2one('create.clreparation_mrp')
 
-    ureparation = fields.One2many(
-        'cl.reparation', 'reparation_test_user', 'Reparacion')
-    tname = fields.Char("Test                       ")
-    notes = fields.Char("Observaciones")
-    yes = fields.Boolean("Si")
-    no = fields.Boolean("No")
-
-
-class getmrpdata(models.TransientModel):
-    _name = 'getmrp.data.basic'
-    _description = "Get MRP Repair basic Data"
-
-    new_line_id_b = fields.Many2one('create.clreparation_mrp')
-
-    ureparation = fields.One2many(
-        'cl.reparation', 'reparation_test_user', 'Reparacion')
-    tname = fields.Char("Test                       ")
+    ureparation = fields.One2many('cl.reparation', 'reparation_test', 'Reparacion')
+    breparation = fields.One2many('cl.reparation', 'reparation_test', 'Reparacion')
+    name = fields.Char("Test                       ")
     notes = fields.Char("Observaciones")
     yes = fields.Boolean("Si")
     no = fields.Boolean("No")
