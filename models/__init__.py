dd7fc6eb8d201fb5279c964c70dbd4a98fc083ba
monik43 <monimons666@gmail.com>
2021-08-31 12:34:14 +0200

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-
+from . import mrp_repair
 from . import helpdesk
 
 from . import mail_activity
 from . import reparation
-from . import mrp_repair
+
 from . import user_credentials
 from . import stock_production_lot
 from . import stock_picking
