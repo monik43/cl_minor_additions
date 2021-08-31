525c795a0adb9d5ceadf743745f291922130e033
monik43 <monimons666@gmail.com>
2021-08-31 10:24:43 +0200

    visibilidad en tree, computed test pasado

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from . import user_credentials
 from . import reparation
 from . import mrp_repair
-from . import user_credentials
 from . import stock_picking
 from . import stock_move
 from . import stock_production_lot
