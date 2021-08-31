99d358f9466ab920a88d36a26bcb8ec05f3432dc
monik43 <monimons666@gmail.com>
2021-08-31 11:45:15 +0200

    test

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ from . import reparation
 from . import mrp_repair
 from . import stock_production_lot
 from . import mail_activity
+
+
+from . import helpdesk
 from . import stock_picking
 from . import stock_move
-from . import helpdesk
 from . import user_credentials
 from . import purchase
 from . import create_purchaseorder_mrp
