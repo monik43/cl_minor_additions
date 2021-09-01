d03c2cbbde3f946e415f063cccbee6549025c347
monik43 <monimons666@gmail.com>
2021-09-01 09:25:28 +0200

    test

--- __manifest__.py
+++ __manifest__.py
@@ -15,22 +15,19 @@
     'version': '11.0.0.6',
     'depends': ['base', 'stock','mrp_repair', 'helpdesk', 'account', 'cloudedu_mods', 'filtro_comercial-main'],
     'data': [
-        'wizard/create_reparation.xml',
 
         'views/web_assets.xml',
         'views/account_invoice_views.xml',
         'views/account_templates.xml',
         'views/report_picking_final_2.xml',
         ##'data/user_credentials.xml',
-        
-        
         'views/purchase_order_mrp_wiz.xml',
-
         'views/mail_views.xml',
         'views/mrp_repair_views.xml',
         'views/helpdesk_views.xml',
         'views/stock_views.xml',
         'views/reparation_views.xml',
+        'wizard/create_reparation.xml',
     ],
     'qweb': [
         'static/src/xml/account_payment.xml',
