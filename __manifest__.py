7d4c9c464533f2e4c727282f2e39f6e488973f3d
monik43 <monimons666@gmail.com>
2021-09-01 09:28:38 +0200

    test

--- __manifest__.py
+++ __manifest__.py
@@ -22,8 +22,9 @@
         'views/report_picking_final_2.xml',
         ##'data/user_credentials.xml',
         'views/purchase_order_mrp_wiz.xml',
-        'views/mail_views.xml',
         'views/mrp_repair_views.xml',
+
+        'views/mail_views.xml',
         'views/helpdesk_views.xml',
         'views/stock_views.xml',
         'views/reparation_views.xml',
