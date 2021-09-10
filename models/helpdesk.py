3d59dda15f26a299e57b4ad465a96d4b9d15ec92
monik43 <monimons666@gmail.com>
2021-09-10 12:26:46 +0200

    prueba

--- models/helpdesk.py
+++ models/helpdesk.py
@@ -3,7 +3,7 @@
 import datetime
 from dateutil import relativedelta
 from odoo import api, fields, models, _
-
+from odoo.exceptions import Warning
 
 class helpdesk_stage(models.Model):
     _inherit = "helpdesk.stage"
@@ -45,21 +45,24 @@ class helpdesk_ticket(models.Model):
                 rec.ordensat = rec.env['mrp.repair'].search(
                     [('x_ticket', '=', rec.id)])
             elif rec.stage_id.name == 'Asignado':
-                vals = {
-                    'x_ticket': rec.id,
-                    #'product_id': rec.prod_id_context.id,
-                    'n_lot_id': rec.lot_id_context.id,
-                    'name': rec.name_rma,
-                    'partner_id': rec.partner_id.id,
-                    #'product_qty': 1,
-                    #'product_uom': rec.prod_id_context.uom_id.id,
-                    'company_id':1,
-                    'invoice_method':'none',
-                    'pricelist_id': 1,
-                    'internal_notes': "Reparación creada cuando el estado del ticket relacionado se cambió a \"Asignado\"."
-                }
-                repar = rec.env['mrp.repair'].create(vals)
-                rec.ordensat = repar
+                if rec.x_lot_id != False:
+                    vals = {
+                        'x_ticket': rec.id,
+                        'product_id': rec.prod_id_context.id,
+                        'n_lot_id': rec.lot_id_context.id,
+                        'name': rec.name_rma,
+                        'partner_id': rec.partner_id.id,
+                        'product_qty': 1,
+                        'product_uom': rec.prod_id_context.uom_id.id,
+                        'company_id':1,
+                        'invoice_method':'none',
+                        'pricelist_id': 1,
+                        'internal_notes': "Reparación creada cuando el estado del ticket relacionado se cambió a \"Asignado\"."
+                    }
+                    repar = rec.env['mrp.repair'].create(vals)
+                    rec.ordensat = repar
+                else:
+                    raise Warning(_("No se ha registrado el número de serie en la base de datos. Antes de cambiar el estado, registre el nº de serie."))
 
     def _get_name_rma(self):
         for rec in self:
