9679623189afcc355133431bf05ad7eac0e65149
monik43 <monimons666@gmail.com>
2021-09-13 09:40:16 +0200

    js

--- static/src/js/web_relational_fields.js
+++ static/src/js/web_relational_fields.js
@@ -1,60 +1,68 @@
-odoo.define('cl_minor_additions.confirm_stage_change', function (require) {
-    "use strict";
+odoo.define("cl_minor_additions.confirm_stage_change", function (require) {
+  "use strict";
 
-    var relational_fields = require('web.relational_fields');
+  var relational_fields = require("web.relational_fields");
 
-    var core = require('web.core');
-    var Dialog = require('web.Dialog');
-    var _rpc = require('web.rpc');
-    var _t = core._t;
+  var core = require("web.core");
+  var Dialog = require("web.Dialog");
+  var _rpc = require("web.rpc");
+  var _t = core._t;
 
-    relational_fields.FieldStatus.include({
-        /**
-         * Called when on status stage is clicked -> sets the field value.
-         * @private
-         * @param {MouseEvent} e
-         */
-        _onClickStage: function (e) {
-            var self = this;
-            var target = $(e.currentTarget).data("value");
-            if (self.model == 'helpdesk.ticket'){
-                _rpc.query({
-                    model: 'helpdesk.stage',
-                    method: 'js_template_handler',
-                    args: [target]
-                }).then(function (data) {
-                    if (data[0] != false) {
-                        console.log(data)
-                        if (data[1] == "Asignado"){
-                            _rpc.query({
-                                model: 'helpdesk.ticket',
-                                method: 'js_stage_handler',
-                                args: [target]
-                            }).then(function (data2) {
-                                if (data2 == false){
-                                    Dialog.confirm(this, _t("No se ha registrado el número de serie en la base de datos. Antes de cambiar el estado, registre el nº de serie."), {
-                                        confirm_callback: function () {
-                                            console.log(data2);
-                                        },
-                                    });
-                                }
-                            }
-                        } else {
-                            Dialog.confirm(this, _t("La etapa a la  que estás intentando cambiar tiene una plantilla de mail. Estás segurx de que quieres cambiar a esa etapa?"), {
-                                confirm_callback: function () {
-                                    self._setValue(target);
-                                },
-                            });
-                        }
-                        
-                    } else {
-                        self._setValue(target);
+  relational_fields.FieldStatus.include({
+    /**
+     * Called when on status stage is clicked -> sets the field value.
+     * @private
+     * @param {MouseEvent} e
+     */
+    _onClickStage: function (e) {
+      var self = this;
+      var target = $(e.currentTarget).data("value");
+      if (self.model == "helpdesk.ticket") {
+        _rpc
+          .query({
+            model: "helpdesk.stage",
+            method: "js_template_handler",
+            args: [target],
+          })
+          .then(function (data) {
+            if (data[0] != false) {
+              console.log(data);
+              if (data[1] != "Asignado") {
+                _rpc
+                  .query({
+                    model: "helpdesk.ticket",
+                    method: "js_stage_handler",
+                    args: [target],
+                  })
+                  .then(function (data2) {
+                    if (data2 == false) {
+                      Dialog.confirm(this, _t("No se ha registrado el número de serie en la base de datos. Antes de cambiar el estado, registre el nº de serie."), {
+                        confirm_callback: function () {
+                          console.log(data2);
+                        },
+                      });
                     }
-                });
+                  });
+              } else {
+                Dialog.confirm(
+                  this,
+                  _t(
+                    "La etapa a la que estás intentando cambiar tiene una plantilla de mail. Estás segurx de que quieres cambiar a esa etapa?"
+                  ),
+                  {
+                    confirm_callback: function () {
+                      self._setValue(target);
+                    },
+                  }
+                );
+              }
             } else {
-                self._setValue(target);
+              self._setValue(target);
             }
-            
-        },
-    });
+          });
+      } else {
+        self._setValue(target);
+      }
+    },
+  });
 });
