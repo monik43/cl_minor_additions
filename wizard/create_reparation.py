1576e333ef94aa24438eef8a3983a2467aab910e
monik43 <monimons666@gmail.com>
2021-09-13 12:13:28 +0200

    form

--- wizard/create_reparation.py
+++ wizard/create_reparation.py
@@ -102,7 +102,7 @@ class createclreparation_mrp(models.TransientModel):
             self._context.get('active_ids', []))
         res.update({'origen_rep': data.id})
         basic_test_names = []
-        for line in self.env['cl.default.newtest'].search():
+        for line in self.env['cl.default.newtest']:
             print(line.name)
         #for line in 
         """if data.product_id.id in (3412, 1279, 3405, 104, 1227, 242, 3379, 19, 400, 3165, 403, 3102, 3247, 1276, 3365, 3364, 3086, 297, 324, 330):
