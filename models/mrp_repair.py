9d508262e47e6ee868d31905a362149ba4df318e
monik43 <monimons666@gmail.com>
2021-09-16 12:12:23 +0200

    fix

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -85,5 +85,5 @@ class mrp_repair(models.Model):
             'view_mode': 'form',
             'res_model': self._name,
             'res_id': self.id,
-            'target': 'current'
+            'target': 'new'
         }
