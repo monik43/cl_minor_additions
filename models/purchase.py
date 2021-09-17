2ddb951307ce0cc3f2a347ba9a81f75741d8d936
monik43 <monimons666@gmail.com>
2021-09-17 11:27:14 +0200

    fix test

--- models/purchase.py
+++ models/purchase.py
@@ -5,6 +5,13 @@ from odoo import models, fields, api, _
 class purchase_order(models.Model):
     _inherit = 'purchase.order'
 
+    pid = fields.Char(compute="_get_pid")
+
+    def _get_pid(self):
+        for rec in self:
+            rec.pid = rec.origin[4:]
+            print(rec.pid)
+
     @api.onchange('order_line')
     def update_order_lines_fields(self):
         for rec in self:
