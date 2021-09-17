5cf8e4bfe3d16692012ff132383e59181a5b6bc2
monik43 <monimons666@gmail.com>
2021-09-17 11:49:41 +0200

    fix test

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -56,7 +56,7 @@ class mrp_repair(models.Model):
 
     def _get_purchase_orders(self):
         for rec in self:
-            for line in rec.env['purchase.order'].search([('partner_ref'[:4], '=', rec.x_ticket.id)]):
+            for line in rec.env['purchase.order'].search([('partner_ref', 'like', rec.x_ticket.id)]):
                 print(line, " ", line.partner_ref)
                 #rec.update({'purchase_orders':[(4, line.id)]})
                 
