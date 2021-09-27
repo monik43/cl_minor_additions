ef24fdfa977dda0373f4b8c49d4bba17101b53a0
monik43 <monimons666@gmail.com>
2021-09-27 13:00:52 +0200

    test

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -53,7 +53,7 @@ class mrp_repair(models.Model):
 
     def _get_ticket_x(self):
         for rec in self:
-            if rec.name.startswith('#') and not rec.x_ticket and self.env['helpdesk.ticket'].search([('id','=', rec.name[1:4])]):
+            if rec.name.startswith('#') and not rec.x_ticket and self.env['helpdesk.ticket'].search([('id','=', rec.name[1:5])]):
                 print(self.env['helpdesk.ticket'].search([('id','=', rec.name[1:4])]), "/"*25)
                 rec.ticket_x = self.env['helpdesk.ticket'].search([('id','=', rec.name[1:4])])
 
