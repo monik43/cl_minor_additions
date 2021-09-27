8a7775b95bab5668b9c147433a182edd2e893a16
monik43 <monimons666@gmail.com>
2021-09-27 12:46:52 +0200

    test

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -63,7 +63,7 @@ class mrp_repair(models.Model):
                     rec.lot_id = rec.x_ticket.x_lot_id
                 elif rec.x_ticket.x_sn and self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper())]):
                     rec.lot_id = self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper())])
-                if rec.ticket_x:
+            if rec.ticket_x:
                     print(rec.ticket_x)
                     print("test"*25)
 
