d7484abcf459c2efdb19efdd274eda70f791e57a
monik43 <monimons666@gmail.com>
2021-09-28 09:25:53 +0200

    prod id

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -62,12 +62,14 @@ class mrp_repair(models.Model):
                 if rec.x_ticket.x_lot_id:
                     rec.lot_id = rec.x_ticket.x_lot_id
                 elif rec.x_ticket.x_sn and self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper()),('product_id', '=', rec.product_id.id)]):
-                    rec.lot_id = self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper())])
+                    print(self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper()),('product_id', '=', rec.product_id.id)]))
+                    rec.lot_id = self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper()),('product_id', '=', rec.product_id.id)])
             elif not rec.lot_id and rec.ticket_x:
                 if rec.ticket_x.x_lot_id:
                     rec.lot_id = rec.ticket_x.x_lot_id
                 elif rec.ticket_x.x_sn and self.env['stock.production.lot'].search([('name','=',rec.ticket_x.x_sn[:4].upper()),('product_id', '=', rec.product_id.id)]):
-                    rec.lot_id = self.env['stock.production.lot'].search([('name','=',rec.ticket_x.x_sn.upper())])
+                    print(self.env['stock.production.lot'].search([('name','=',rec.x_ticket.x_sn.upper()),('product_id', '=', rec.product_id.id)]))
+                    rec.lot_id = self.env['stock.production.lot'].search([('name','=',rec.ticket_x.x_sn.upper()),('product_id', '=', rec.product_id.id)])
 
     def _get_purchase_orders(self):
         for rec in self:
