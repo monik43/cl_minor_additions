55c104b7a409395b6445ac06f331d1bdbabc7218
monik43 <monimons666@gmail.com>
2021-09-29 11:01:12 +0200

    prueba ticket perdido

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -60,9 +60,9 @@ class mrp_repair(models.Model):
     def _get_ticket_x(self):
         for rec in self:
             nid = str([c for c in rec.name if c.isdigit()][:4])
-            nid = ''.join(str(c) for c in nid)
-            if not rec.x_ticket and self.env['helpdesk.ticket'].search([('id','=', nid)]):
-                rec.ticket_x = self.env['helpdesk.ticket'].search([('id','=', nid)])
+            sid = ''.join(str(c) for c in nid)
+            if not rec.x_ticket and self.env['helpdesk.ticket'].search([('id','=', sid)]):
+                rec.ticket_x = self.env['helpdesk.ticket'].search([('id','=', sid)])
 
     def _get_lot_id(self):
         for rec in self:
