483ac0beee130f7b129cd1dbd8eece1182deb0bf
monik43 <monimons666@gmail.com>
2021-10-01 10:02:58 +0200

    test

--- static/src/js/activity.js
+++ static/src/js/activity.js
@@ -1,19 +1,43 @@
 odoo.define("cl_minor_additions.newtemplate", function (require) {
   "use strict";
   var mail_activity = require("mail.Activity");
-  //var AbstractField = require("web.AbstractField");
-  //var BasicModel = require("web.BasicModel");
-  var core = require("web.core");
-  //var field_registry = require("web.field_registry");
-  var time = require("web.time");
+  var AbstractField = require("web.AbstractField");
+  var BasicModel = require("web.BasicModel");
+  var field_registry = require("web.field_registry");
   var utils = require("mail.utils");
+  var core = require("web.core");
+  var time = require("web.time");
 
   var QWeb = core.qweb;
   var _t = core._t;
 
   mail_activity.AbstractActivityField.include({
     _render: function () {
-      console.log("test")
+      _.each(this.activities, function (activity) {
+        if (activity.note) {
+          activity.note = utils.parse_and_transform(
+            activity.note,
+            utils.add_link
+          );
+        }
+      });
+      var activities = setDelayLabel(this.activities);
+      if (activities.length) {
+        var nbActivities = _.countBy(activities, "state");
+        console.log("TEST");
+        this.$el.html(
+          QWeb.render("ActivityItemsCl", {
+            activities: activities,
+            nbPlannedActivities: nbActivities.planned,
+            nbTodayActivities: nbActivities.today,
+            nbOverdueActivities: nbActivities.overdue,
+            date_format: time.getLangDateFormat(),
+            datetime_format: time.getLangDatetimeFormat(),
+          })
+        );
+      } else {
+        this.$el.empty();
+      }
     },
   });
 });
