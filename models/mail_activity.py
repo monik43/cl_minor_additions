d4e2a0a5a4d8b4af3c950880d426487a40eab4ce
monik43 <monimons666@gmail.com>
2021-10-01 11:16:20 +0200

    test

--- models/mail_activity.py
+++ models/mail_activity.py
@@ -7,8 +7,6 @@ class mail_activity(models.Model):
 
     filtro_check = fields.Boolean('Habilitar usuarios externos?')
     create_user_id = fields.Many2one('res.users', 'Creado por',default=lambda self: self.env.user,index=True, required=True)
-    #activity_init_uid = fields.Many2one('res.users')
-    #activity_init_date = fields.Datetime('Fecha creación', required=True, readonly=True)
 
     @api.onchange('filtro_check')
     def onchange_filtro_check(self):
