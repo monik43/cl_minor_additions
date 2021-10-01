e86617d853f836ee9d5099090392b7948a34e88d
monik43 <monimons666@gmail.com>
2021-10-01 09:31:35 +0200

    test

--- static/src/js/mail_activity.js
+++ static/src/js/mail_activity.js
@@ -2,6 +2,7 @@ odoo.define("cl_minor_additions.created_by_user", function (require) {
   "use strict";
   var mail_activity = require("mail.Activity");
   var AbstractField = require("web.AbstractField");
+  var AbstractActivityField = require("mail.AbstractActivityField");
   var BasicModel = require("web.BasicModel");
   var core = require("web.core");
   var field_registry = require("web.field_registry");
@@ -11,34 +12,32 @@ odoo.define("cl_minor_additions.created_by_user", function (require) {
   var QWeb = core.qweb;
   var _t = core._t;
 
-  mail_activity.include({
-  AbstractActivityField = AbstractField.extend({
-      _render: function () {
-        _.each(this.activities, function (activity) {
-          if (activity.note) {
-            activity.note = utils.parse_and_transform(
-              activity.note,
-              utils.add_link
-            );
-          }
-        });
-        var activities = setDelayLabel(this.activities);
-        if (activities.length) {
-          var nbActivities = _.countBy(activities, "state");
-          this.$el.html(
-            QWeb.render("activity_items", {
-              activities: activities,
-              nbPlannedActivities: nbActivities.planned,
-              nbTodayActivities: nbActivities.today,
-              nbOverdueActivities: nbActivities.overdue,
-              date_format: time.getLangDateFormat(),
-              datetime_format: time.getLangDatetimeFormat(),
-            })
+  mail_activity.AbstractActivityField.extend({
+    _render: function () {
+      _.each(this.activities, function (activity) {
+        if (activity.note) {
+          activity.note = utils.parse_and_transform(
+            activity.note,
+            utils.add_link
           );
-        } else {
-          this.$el.empty();
         }
-      },
-    }),
+      });
+      var activities = setDelayLabel(this.activities);
+      if (activities.length) {
+        var nbActivities = _.countBy(activities, "state");
+        this.$el.html(
+          QWeb.render("activity_items", {
+            activities: activities,
+            nbPlannedActivities: nbActivities.planned,
+            nbTodayActivities: nbActivities.today,
+            nbOverdueActivities: nbActivities.overdue,
+            date_format: time.getLangDateFormat(),
+            datetime_format: time.getLangDatetimeFormat(),
+          })
+        );
+      } else {
+        this.$el.empty();
+      }
+    },
   });
 });
