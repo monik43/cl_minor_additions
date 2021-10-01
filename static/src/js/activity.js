0ee1deff816f73893b0d903cb6908072ceaf7e70
monik43 <monimons666@gmail.com>
2021-10-01 09:58:24 +0200

    test

--- static/src/js/activity.js
+++ static/src/js/activity.js
@@ -13,31 +13,7 @@ odoo.define("cl_minor_additions.newtemplate", function (require) {
 
   mail_activity.AbstractActivityField.include({
     _render: function () {
-      _.each(this.activities, function (activity) {
-        if (activity.note) {
-          activity.note = utils.parse_and_transform(
-            activity.note,
-            utils.add_link
-          );
-        }
-      });
-      var activities = setDelayLabel(this.activities);
-      if (activities.length) {
-        var nbActivities = _.countBy(activities, "state");
-        console.log("TEST")
-        this.$el.html(
-          QWeb.render("ActivityItemsCl", {
-            activities: activities,
-            nbPlannedActivities: nbActivities.planned,
-            nbTodayActivities: nbActivities.today,
-            nbOverdueActivities: nbActivities.overdue,
-            date_format: time.getLangDateFormat(),
-            datetime_format: time.getLangDatetimeFormat(),
-          })
-        );
-      } else {
-        this.$el.empty();
-      }
+      console.log("test")
     },
   });
 });
