06cbff52f4b99c3a13e0468cecf8ff23e8ac39cb
monik43 <monimons666@gmail.com>
2021-10-20 09:46:14 +0200

    test x_ticket mrp

--- models/mrp_repair.py
+++ models/mrp_repair.py
@@ -6,13 +6,13 @@ from odoo.exceptions import UserError
 
 
 class repair_line(models.Model):
-    _inherit = 'mrp.repair.line'
+    _inherit = "mrp.repair.line"
 
-    pieza_anyadida = fields.Boolean('Pieza añadida?')
+    pieza_anyadida = fields.Boolean("Pieza añadida?")
 
-    @api.onchange('type', 'repair_id')
+    @api.onchange("type", "repair_id")
     def onchange_operation_type(self):
-        """ On change of operation type it sets source location, destination location
+        """On change of operation type it sets source location, destination location
         and to invoice field.
         @param product: Changed operation type.
         @param guarantee_limit: Guarantee limit of current record.
@@ -21,35 +21,55 @@ class repair_line(models.Model):
         if not self.type:
             self.location_id = False
             self.location_dest_id = False
-        elif self.type == 'add':
+        elif self.type == "add":
             self.onchange_product_id()
-            args = self.repair_id.company_id and [
-                ('company_id', '=', self.repair_id.company_id.id)] or []
-            warehouse = self.env['stock.warehouse'].search(args, limit=1)
+            args = (
+                self.repair_id.company_id
+                and [("company_id", "=", self.repair_id.company_id.id)]
+                or []
+            )
+            warehouse = self.env["stock.warehouse"].search(args, limit=1)
             self.location_id = self.repair_id.location_id
-            self.location_dest_id = self.env['stock.location'].search(
-                [('usage', '=', 'production')], limit=1).id
+            self.location_dest_id = (
+                self.env["stock.location"]
+                .search([("usage", "=", "production")], limit=1)
+                .id
+            )
         else:
             self.price_unit = 0.0
             self.tax_id = False
-            self.location_id = self.env['stock.location'].search(
-                [('usage', '=', 'production')], limit=1).id
-            self.location_dest_id = self.env['stock.location'].search(
-                [('scrap_location', '=', True)], limit=1).id
+            self.location_id = (
+                self.env["stock.location"]
+                .search([("usage", "=", "production")], limit=1)
+                .id
+            )
+            self.location_dest_id = (
+                self.env["stock.location"]
+                .search([("scrap_location", "=", True)], limit=1)
+                .id
+            )
 
 
 class mrp_repair(models.Model):
-    _inherit = 'mrp.repair'
+    _inherit = "mrp.repair"
 
-    ticket_x = fields.Many2one('helpdesk.ticket', compute="_get_ticket_x")
-    lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial', domain="[('product_id','=', product_id)]", help="Products repaired are all belonging to this lot", oldname="prodlot_id")
+    ticket_x = fields.Many2one("helpdesk.ticket", compute="_get_ticket_x")
+    lot_id = fields.Many2one(
+        "stock.production.lot",
+        "Lot/Serial",
+        domain="[('product_id','=', product_id)]",
+        help="Products repaired are all belonging to this lot",
+        oldname="prodlot_id",
+    )
     po_rel = fields.Boolean(compute="_compute_po_rel")
     test_end = fields.Boolean(compute="_get_test_end")
     rep_conf = fields.Boolean(default=False, compute="_get_state")
-    rec = fields.Many2one('mrp.repair', compute="_get_rec")
+    rec = fields.Many2one("mrp.repair", compute="_get_rec")
     rma = fields.Char(compute="_get_rma")
-    reparation = fields.One2many('cl.reparation', 'origen_rep', "Reparaciones")
-    purchase_orders = fields.Many2many('purchase.order', compute="_get_purchase_orders", ondelete='set null')
+    reparation = fields.One2many("cl.reparation", "origen_rep", "Reparaciones")
+    purchase_orders = fields.Many2many(
+        "purchase.order", compute="_get_purchase_orders", ondelete="set null"
+    )
     sn_x = fields.Char(compute="_get_sn_x", store=True)
 
     def _get_sn_x(self):
@@ -59,7 +79,7 @@ class mrp_repair(models.Model):
 
     def _get_ticket_x(self):
         for rec in self:
-            name = rec.name[:10].replace(" ","")
+            name = rec.name[:10].replace(" ", "")
             s = 0
             chars = False
 
@@ -75,18 +95,24 @@ class mrp_repair(models.Model):
             while s in range(len(name)):
                 if not name[s].isdigit():
                     chars = True
-                s=s+1
-            print(f"chars: {chars}, hd search: {self.env['helpdesk.ticket'].search([('id','=', name)])}, hd search lot: {self.env['helpdesk.ticket'].search([('id','=', name)]).x_lot_id}")
-            if not chars and self.env['helpdesk.ticket'].search([('id','=', name)]):
-                rec.ticket_x = self.env['helpdesk.ticket'].search([('id','=', name)])
+                s = s + 1
+            print(
+                f"chars: {chars}, hd search: {self.env['helpdesk.ticket'].search([('id','=', name)])}, hd search lot: {self.env['helpdesk.ticket'].search([('id','=', name)]).x_lot_id}"
+            )
+            if not chars and self.env["helpdesk.ticket"].search([("id", "=", name)]):
+                print("A"*10)
+                rec.ticket_x = self.env["helpdesk.ticket"].search([("id", "=", name)])
                 if rec.ticket_x.x_lot_id and not rec.lot_id:
+                    print("B"*10)
                     rec.lot_id = rec.ticket_x.x_lot_id
 
     def _get_purchase_orders(self):
         for rec in self:
-            for line in rec.env['purchase.order'].search([('partner_ref', '=', rec.name)]):
-                rec.update({'purchase_orders':[(4, line.id)]})
-                
+            for line in rec.env["purchase.order"].search(
+                [("partner_ref", "=", rec.name)]
+            ):
+                rec.update({"purchase_orders": [(4, line.id)]})
+
     def _get_test_end(self):
         for rec in self:
             for line in rec.reparation:
@@ -105,31 +131,34 @@ class mrp_repair(models.Model):
 
     def _compute_po_rel(self):
         for rec in self:
-            if rec.env['purchase.order'].search([('partner_ref', 'like', rec.name)]) != False:
+            if (
+                rec.env["purchase.order"].search([("partner_ref", "like", rec.name)])
+                != False
+            ):
                 rec.po_rel = True
 
     def _get_state(self):
         for rec in self:
-            if rec.state == 'confirmed' and rec.rep_conf != True:
+            if rec.state == "confirmed" and rec.rep_conf != True:
                 rec.rep_conf = True
 
     @api.model
-    def default_get(self,  fields):
+    def default_get(self, fields):
         res = super(mrp_repair, self).default_get(fields)
         return res
 
     @api.multi
     def open_act(self):
         for rec in self:
-            url = self.env['ir.config_parameter'].sudo().get_param(
-                'web.base.url')
-            rec_url = url + "/web#id=" + \
-                str(self.id) + "&view_type=form&model=mrp.repair"
+            url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+            rec_url = (
+                url + "/web#id=" + str(self.id) + "&view_type=form&model=mrp.repair"
+            )
             client_action = {
-                'type': 'ir.actions.act_url',
-                'name': self.display_name,
-                'target': 'new',
-                'url': rec_url,
+                "type": "ir.actions.act_url",
+                "name": self.display_name,
+                "target": "new",
+                "url": rec_url,
             }
 
             return client_action
