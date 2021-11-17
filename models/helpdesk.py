f9f3783be5283f8e8d6ca4ca0ed52f7f601b48ed
monik433 <monimons666@gmail.com>
2021-11-17 10:37:03 +0100

    mail template enabler & disabler

--- models/helpdesk.py
+++ models/helpdesk.py
@@ -33,15 +33,8 @@ class helpdesk_stage(models.Model):
     #@api.model
     @api.onchange('template_id')
     def onchange_template_id(self):
-        print(f"""
-            template -> {self.template_id}
-            backup   -> {self.template_backup}
-        """)
-            #if self.template_backup:
-            #print("start\n")
-            #time.sleep(10.5)
-            #print("end\n")
-            #self.template_id = self.template_backup
+        if self.template_backup:
+            self.template_id = self.template_backup
 
     @api.model
     def js_mail_template_disabler(self, rec_id):
