19b5eedbb74ad3c656a66d0117edd86bed7a07d0
monik433 <monimons666@gmail.com>
2021-11-17 09:40:40 +0100

    test mail template disabler

--- static/src/js/web_relational_fields.js
+++ static/src/js/web_relational_fields.js
@@ -81,6 +81,15 @@ odoo.define("cl_minor_additions.confirm_stage_change", function (require) {
                                 console.log("mail template disabler")
                               });
                             self._setValue(target);
+                            _rpc
+                              .query({
+                                model: "helpdesk.stage",
+                                method: "js_mail_template_enabler",
+                                args: [target],
+                              })
+                              .then(function (data4) {
+                                console.log("mail template enabler")
+                              });
                           },
                         }
                       );
