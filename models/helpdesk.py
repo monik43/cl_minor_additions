1b8fce99aec6bb7951dba987c10c62ad3c680c02
monik433 <monimons666@gmail.com>
2021-11-17 10:38:56 +0100

    mail template enabler & disabler

--- models/helpdesk.py
+++ models/helpdesk.py
@@ -17,6 +17,9 @@ class helpdesk_stage(models.Model):
     )
     
     def _compute_template_backup(self):
+        print(f'''
+            /{str(self.template_id)}
+        ''')
         if str(self.template_id) != "mail.template()":
             print("self.template_id != False, ", self.template_id)
             self.template_backup = self.template_id
